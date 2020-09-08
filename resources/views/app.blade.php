be1fa11119552665faadf23ac69b2e754d6d270f
junpei5060 <junpei.nakazawa.5060@gmail.com>
2020-09-08 11:32:16 +0900

    tasklist完了

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,6 +16,7 @@
 <div id="app">
 <!-- <example-component></example-component> -->
 <header-component></header-component>
+<router-view></router-view>
 </div>
 <!-- Scripts -->
 <script src="{{ mix('/js/app.js') }}" defer></script>
